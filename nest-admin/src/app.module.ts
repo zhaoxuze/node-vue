82a3e0acef0627241b4bc1be0678ef364fb6ec49
john <john.zhao@newtype.io>
2019-02-27 08:37:39 +0800

    测试数据库连接保存

--- nest-admin/src/app.module.ts
+++ nest-admin/src/app.module.ts
@@ -7,16 +7,7 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'abc@123',
-      database: 'admin',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(),
     UserModule
   ]
 })
--- nest-admin/src/user/user.service.ts
+++ nest-admin/src/user/user.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import {createConnection} from "typeorm";
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, getManager, getConnection } from 'typeorm';
 import { Users } from "../entities/user.entity";
 
 @Injectable()
 export class UserService {
+  constructor(@InjectRepository(Users) private readonly userRepository: Repository<Users>) { };
   getHello() {
     // return 'Hello World!';
-      createConnection(/*...*/).then(connection => {
-
       let usr = new Users();
       usr.admin_name = "John";
       usr.admin_passwd = "123456";
@@ -15,13 +16,12 @@ export class UserService {
       usr.updated_at = 1;
       usr.admin_status = 1;
 
-      return connection.manager
+      return this.userRepository.manager
               .save(usr)
               .then(photo => {
                   console.log("user is saved", photo.id);
                   return 'aaaa';
               });
 
-    }).catch(error => console.log(error));
   }
 }
