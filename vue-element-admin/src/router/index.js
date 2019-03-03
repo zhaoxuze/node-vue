38372056d096ec7302bbc7e73d88c4c7e3f99715
john <john.zhao@newtype.io>
2019-03-03 08:32:58 +0800

    首页修改

--- vue-element-admin/src/router/index.js
+++ vue-element-admin/src/router/index.js
@@ -8,9 +8,7 @@ import Layout from '@/views/layout/Layout'
 
 /* Router Modules */
 import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
 import tableRouter from './modules/table'
-import nestedRouter from './modules/nested'
 
 /** note: Submenu only appear when children.length>=1
  *  detail see  https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
@@ -88,33 +86,7 @@ export const asyncRouterMap = [
 
   /** When your routing table is too long, you can split it into small modules**/
   componentsRouter,
-  chartsRouter,
-  nestedRouter,
   tableRouter,
-  {
-    path: '/error',
-    component: Layout,
-    redirect: 'noredirect',
-    name: 'ErrorPages',
-    meta: {
-      title: 'errorPages',
-      icon: '404'
-    },
-    children: [
-      {
-        path: '401',
-        component: () => import('@/views/errorPage/401'),
-        name: 'Page401',
-        meta: { title: 'page401', noCache: true }
-      },
-      {
-        path: '404',
-        component: () => import('@/views/errorPage/404'),
-        name: 'Page404',
-        meta: { title: 'page404', noCache: true }
-      }
-    ]
-  },
 
   { path: '*', redirect: '/404', hidden: true }
 ]
